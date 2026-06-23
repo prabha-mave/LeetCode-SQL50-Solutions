39e945d224768a62d97ebb73be60b4358fe69e4f
Prabha R <prabhadevid@gmail.com>
2026-06-24 02:06:30 +0530

    Remove Pandas solutions

--- 2883-drop-missing-data/2883-drop-missing-data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    students = students.dropna(axis=0, how ='any',subset = 'name', inplace=False, ignore_index=False)
-    return students
