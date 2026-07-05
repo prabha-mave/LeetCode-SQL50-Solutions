e33ae2cc52b744cde3f5532ae3ed8f237523e4e4
Prabhadevi R <prabhadeviid@gmail.com>
2026-07-05 19:46:26 +0530

    Time: 1651 ms (37.21%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,30 @@
+/* Write your T-SQL query statement below */
+
+select 
+    format(trans_date, 'yyyy-MM') as month,
+    country,
+
+    count(id) as trans_count,
+
+    count(
+        case 
+            when state = 'approved' 
+            then 1 
+        end
+    ) as approved_count,
+
+    sum(amount) as trans_total_amount,
+
+    sum(
+        case 
+            when state = 'approved' 
+            then amount 
+            else 0 
+        end
+    ) as approved_total_amount
+
+from transactions
+
+group by 
+    format(trans_date, 'yyyy-MM'),
+    country
