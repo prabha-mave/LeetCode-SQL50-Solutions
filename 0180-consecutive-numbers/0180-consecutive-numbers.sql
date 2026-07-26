e1077fad181b96686b911e8c0a2cd3305e3c6761
Prabhadevi R <prabhadeviid@gmail.com>
2026-07-26 15:32:31 +0530

    Time: 351 ms (10.15%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,8 +1,19 @@
 -- Write your PostgreSQL query statement below
-select distinct l1.num as ConsecutiveNums 
+/*select distinct l1.num as ConsecutiveNums 
 from logs l1
 join logs l2 on l2.id = l1.id+1
 join logs l3
 on l3.id = l2.id+1
 where l1.num = l2.num
-and l2.num = l3.num
+and l2.num = l3.num */
+
+with cte as (
+    select num,
+    lead(num,1) over (order by id) as next_num,
+    lead(num,2) over (order by id) as next_next_num
+    from logs
+)
+select distinct num as ConsecutiveNums 
+from cte
+where num = next_num
+and num = next_next_num
