740c8ac8c1bc6f6a1070e860c4859689febbfeea
Prabhadevi R <prabhadeviid@gmail.com>
2026-07-31 06:45:12 +0530

    Time: 171 ms (94.83%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,22 @@
+-- Write your PostgreSQL query statement below
+/*
+with cte as(
+    select requester_id as user_id
+    from requestaccepted
+    union all
+    select accepter_id as user_id
+    from requestaccepted)
+select user_id as id, count(user_id) as num
+from cte
+group by user_id
+order by num desc
+limit 1*/
+
+
+
+select sub.id, count(sub.id) as num
+from requestaccepted,
+    lateral (values(requester_id), (accepter_id)) as sub(id)
+group by sub.id
+order by num desc
+limit 1
